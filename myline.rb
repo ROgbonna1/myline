68a7e16f7f6acb8787e138bc7408e45b310f9502
Reuben Ogbonna <reubenogbonna@gmail.com>
2019-02-06 17:13:09 +0000

    first take a restaurant search algorithm

--- myline.rb
+++ myline.rb
@@ -5,36 +5,70 @@ require 'net/http'
 require 'uri'
 require 'json'
 
-# Setup global parameters
-GoogleMapsService.configure do |config|
-  config.key = 'AIzaSyARPpg-mcc-UvyecsGreHSHfYfshI87epU'
-  config.retry_timeout = 20
-  config.queries_per_second = 10
+
+def yelp_search(search_params = {limit: 10, location: 'New York, NY'}) # Grabs JSON from Yelp Fusion API call
+  uri = URI('https://api.yelp.com/v3/businesses/search') # base uri for yelp api
+  parameters = { 'term' => 'restaurant', 'limit' => search_params[:limit], 'sort_by' => 'rating', 'location' => search_params[:location] } # search parameters
+  uri.query = URI.encode_www_form(parameters) # the .encode_www_form methods formats my parameters hash into query parameter format. The .query method sets this as the query for my uri.
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  restaurants = http.get(uri.request_uri, initheader = { 'Authorization' => 'Bearer wPydcDY5ij_piBDddbiBECp2bE4PUR1rDqjcfsZbRF8aVQd1TONWk6Vlyu837T3HvQrSLxejSN4RQYz2ds-rdgghQz8AFIPtJ1J37oAUHt-BJxz9mN3nyCLnBU5aXHYx' })
+  results = JSON.parse(restaurants.body, symbolize_names: true)
+  results[:businesses]
 end
 
-# Initialize client using global parameters
-gmaps = GoogleMapsService::Client.new
+def yelp_list_businesses(results, search_params = { limit: 50})
+  list = results[:businesses].map do |business|
+    { 
+      name: business[:name], 
+      location: [business[:coordinates][:latitude], business[:coordinates][:longitude]],
+      url: business[:url]
+    }
+  end
+  list[0...search_params[:limit]]
+end
+
+
+def travel_delta(location1, location2, destination, travel_mode)
+  distance_from_location1 = trip_time(destination, location1, travel_mode)
+  distance_from_location2 = trip_time(destination, location2, travel_mode)
+  (distance_from_location1 - distance_from_location2).abs
+end
+
+helpers do
+  def google_directions(origin, destination)
+    uri = URI('https://www.google.com/maps/dir/?api=1')
+    parameters = { 'api' => '1', 'origin' => origin, 'destination' => destination }
+    uri.query = URI.encode_www_form(parameters)
+    uri.to_s
+  end
+  
+  def trip_time(to, from, travel_mode)
+    gmaps = GoogleMapsService::Client.new(key: 'AIzaSyARPpg-mcc-UvyecsGreHSHfYfshI87epU')
+    trip = gmaps.directions(to, from, mode: travel_mode, alternatives: false).first
+    trip[:legs].first[:duration][:value]
+  end
+  
+  def coordinates(business)
+    [business[:coordinates][:latitude], business[:coordinates][:longitude] ]
+  end
+end
 
 get "/" do
   erb :index
 end
 
 post "/search" do
-  from = params[:origin]
-  to = params[:destination]
+  @location1 = params[:location1]
+  @location2 = params[:location2]
   mode = params[:mode]
-  results = gmaps.directions(to, from, mode: mode, alternatives: false).first
-  @duration = results[:legs].first[:duration][:text]
-  @distance = results[:legs].first[:distance][:text]
+  
+  restaurants = (yelp_search(location: @location1, limit: 10) + 
+                  yelp_search(location: @location2, limit: 10)).uniq
+  
+  @restaurants = restaurants.sort_by do |restaurant|
+    travel_delta(@location1, @location2, coordinates(restaurant), mode)
+  end
+  
   erb :results
-  
-  # Grabbing JSON from Yelp Fusion API call
-  uri = URI('https://api.yelp.com/v3/businesses/search') # base uri for yelp api
-  parameters = { 'term' => 'restaurant', 'location' => from } # search parameters
-  uri.query = URI.encode_www_form(parameters) # the .encode_www_form methods formats my parameters hash into query parameter format. The .query method sets this as the query for my uri.
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true
-  restaurants = http.get(uri.request_uri, initheader = { 'Authorization' => 'Bearer wPydcDY5ij_piBDddbiBECp2bE4PUR1rDqjcfsZbRF8aVQd1TONWk6Vlyu837T3HvQrSLxejSN4RQYz2ds-rdgghQz8AFIPtJ1J37oAUHt-BJxz9mN3nyCLnBU5aXHYx' })
-  @restaurants = restaurants.body
-  
 end
